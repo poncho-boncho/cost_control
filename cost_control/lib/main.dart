fbc4139a8233b23d010305f1dcea54261410c571
poncho_boncho <Sorkolas@mail.com>
2024-02-29 10:57:03 +0300

    added ListView display

--- cost_control/lib/main.dart
+++ cost_control/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'transaction.dart';
+import 'package:intl/intl.dart';
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -19,15 +21,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final String defaultLocale = Platform.localeName;
   final List<Transaction> transactions = [
     Transaction(
         id: 't1',
-        title: 'New Shoes',
+        title: 'брюки',
         amount: 2000.200,
         dateTime: DateTime.now()),
     Transaction(
         id: 't2',
-        title: 'New wadwdaw',
+        title: 'Футболка',
         amount: 1200,
         dateTime: DateTime.now())
   ];
@@ -47,7 +50,45 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(children: transactions.map((tx) {
-            return Card(child: Text(tx.title),
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                          color: Colors.black,
+                          width: 2)
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '₽${tx.amount}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(tx.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),),
+                      Text(
+                        DateFormat.yMMMd().format(
+                        tx.dateTime),
+                      style: TextStyle(color: Colors.grey),)
+                    ],
+                  )
+                ],
+                ),
             );
           }).toList(),
           )
